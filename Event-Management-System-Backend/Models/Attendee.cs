1283fa2df1b8bf4ba222f75b70709a7e96c385c4
asirialwis <asirichathurika900@gmail.com>
2025-01-07 20:25:20 +0530

    add model

---
+++ Event-Management-System-Backend/Models/Attendee.cs
@@ -0,0 +1,11 @@
+﻿namespace Event_Management_System_Backend.Models
+{
+    public class Attendee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public List<Event> RegisteredEvents { get; set; } = new List<Event>();
+    }
+}
---
+++ Event-Management-System-Backend/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace Event_Management_System_Backend.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public int RemainingCapacity { get; set; }
+        public string Tags { get; set; } = string.Empty;
+
+        public List<Attendee> Attendees { get; set; } = new List<Attendee>();
+    }
+}
