e0f8d983f9f988b009897bb85bcfb8bb7773744c
asirialwis <asirichathurika900@gmail.com>
2025-01-09 20:43:13 +0530

    add required pages with routing

---
+++ Event-Management-System-Frontend/src/components/EventDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EventDetail() {
+  return (
+    <div>EventDetails</div>
+  )
+}
+
+export default EventDetail
---
+++ Event-Management-System-Frontend/src/components/EventUpdate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EventUpdate() {
+  return (
+    <div>UpdateEvent</div>
+  )
+}
+
+export default EventUpdate
